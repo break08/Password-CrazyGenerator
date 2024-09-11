999fd3e1f8902e11c5176dfd290a6eca24aee781
NoName123 <176148850+break08@users.noreply.github.com>
2024-09-11 20:15:22 +0700

    INSTALL PIP AND PYPERCLIP HERE

---
+++ install_pip_pyperclip.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Make sure you have install python
+echo May be some or all libraries in need you have already had
+pause
+python -m ensurepip --default-pip
+pip install pyperclip
+pause
